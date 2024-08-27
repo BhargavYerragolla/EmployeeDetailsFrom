b7fbe4f45427b821542058e0a3895fd33dce101b
bhargava sai <bhargavsai8887@gmail.com>
2024-08-27 18:20:27 +0530

    First Commit

---
+++ demo/src/main/java/com/example/demo/service/PersonService.java
@@ -0,0 +1,30 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Person;
+import com.example.demo.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PersonService {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    public Person savePerson(Person person) {
+        return personRepository.save(person);
+    }
+
+    public List<Person> getAllPersons() {
+        return personRepository.findAll();
+    }
+  
+    public void deletePersonById(Long id) {
+        personRepository.deleteById(id);
+    }
+
+}
+
